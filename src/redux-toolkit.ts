4d1c12030a181eeea27dbb1d185d70b85b99eb0f
Annie Huang <annie.hn@gmail.com>
2022-01-22 16:37:22 +1100

    Create counterSlice. Shows how the reducers from slice 1 can change state in slice 2 (through extraReducers)

--- src/redux-toolkit.ts
+++ src/redux-toolkit.ts
@@ -89,3 +89,16 @@ const selectedTodoSlice = createSlice({
       payload.id,
   },
 });
+
+// Shows how the reducers from slice 1 can change state in slice 2
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {},
+  extraReducers: {
+    [todosSlice.actions.create.type]: (state) => state + 1,
+    [todosSlice.actions.edit.type]: (state) => state + 1,
+    [todosSlice.actions.toggle.type]: (state) => state + 1,
+    [todosSlice.actions.remove.type]: (state) => state + 1,
+  },
+});
