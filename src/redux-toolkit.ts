8e9cd9b80c934fbbe19237aeb65657e2b3cbdd02
Annie Huang <annie.hn@gmail.com>
2022-01-22 21:28:27 +1100

    reducers will automatically create the actions with matching names

--- src/redux-toolkit.ts
+++ src/redux-toolkit.ts
@@ -265,3 +265,17 @@ const counterSlice = createSlice({
     },
   },
 });*/
+
+/*
+// reducers will automatically create the actions with matching names:
+const counter = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    increment: (state) => state + 1,
+    decrement: (state) => state - 1,
+  },
+});
+counter.actions.increment();
+counter.actions.decrement();
+*/
